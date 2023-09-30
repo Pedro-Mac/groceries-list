dc317532c8e43ff9d0d5c2ce8d64e48ea3012345
Pedro Machado <macpedrot@gmail.com>
2023-09-30 15:44:25 +0100

    🎨 add and retrieve items

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import { addDoc, collection, doc, getDocs, type DocumentData } from 'firebase/firestore'
+import { db } from './config/firebase'
 
+const item = ref('')
+const isLoading = ref(false)
+const itemsList = ref<DocumentData[]>([])
+
+const getList = () => {
+  getDocs(collection(db, "items")).then(querySnapshot => {
+    const a = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+    console.log(a)
+    itemsList.value = a
+  });
+
+  console.log('during promise')
+}
 
 onMounted(() => {
-
+  getList()
 })
+
+
+async function addItem() {
+  try {
+    isLoading.value = true
+    await addDoc(collection(db, "items"), {
+      name: item.value,
+      time: new Date()
+    })
+
+    getList()
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err.message)
+    }
+  } finally {
+    isLoading.value = false
+  }
+
+}
 
 </script>
 
 <template>
   <main>
-    <ul>
+    <form @submit.prevent="addItem">
+      <input type="text" v-model="item" placeholder="Item" :disabled="isLoading" />
+      <button>Add item</button>
+    </form>
 
+    <ul v-if="!isLoading">
+      <li v-for="item in itemsList" :key="item.id">
+        {{ item.name }}
+      </li>
     </ul>
+
   </main>
 </template>
 
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,15 @@
-import { initializeApp } from "firebase/app";
-
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_KEY,
-    authDomain: "vue-shopping-aec41.firebaseapp.com",
-    projectId: "vue-shopping-aec41",
-    storageBucket: "vue-shopping-aec41.appspot.com",
+    authDomain: 'vue-shopping-aec41.firebaseapp.com',
+    projectId: 'vue-shopping-aec41',
+    storageBucket: 'vue-shopping-aec41.appspot.com',
     messagingSenderId: import.meta.env.VITE_FIREBASE_APP_ID,
-    appId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+    appId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
