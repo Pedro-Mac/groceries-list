40c57c5fc694f1d9dac71761f3d144d9b2dde9fe
Pedro Machado <macpedrot@gmail.com>
2023-10-01 20:50:49 +0100

    💄 add button input

---
+++ src/components/base/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button :class="`py-1 px-2 rounded text-base disabled:opacity-80 bg-${color || 'blue'}-400  text-white`">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    color: String,
+    variant: String,
+})
+
+</script>
+
+<style scoped></style>
