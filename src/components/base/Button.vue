b6975140504a927d5aa53bf63776a6db2ac61674
Pedro Machado <macpedrot@gmail.com>
2023-10-01 21:29:54 +0100

    💄 add loader component and loading to button

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -1,13 +1,17 @@
 <template>
     <button :class="`py-1 px-2 rounded text-base disabled:opacity-80 bg-${color || 'blue'}-400  text-white`">
         <slot></slot>
+        <Loader v-if="isLoading" :color="color" :size="16" />
     </button>
 </template>
 
 <script setup lang="ts">
+import Loader from './Loader.vue';
+
 defineProps({
     color: String,
     variant: String,
+    isLoading: Boolean
 })
 
 </script>
---
+++ src/components/base/Loader.vue
@@ -0,0 +1,39 @@
+<template>
+    <span class="loader" :style="{
+        width: `${size}px`,
+        height: `${size}px`,
+
+    }"></span>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+    size: {
+        type: Number,
+        default: 24
+    },
+})
+
+</script>
+
+<style scoped>
+.loader {
+    border-radius: 50%;
+    display: inline-block;
+    border-top: 3px solid #000;
+    border-right: 3px solid transparent;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
