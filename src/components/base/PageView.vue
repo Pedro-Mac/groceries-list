ee3894851bdf2c497f4b9cc0585c40c1060b6e0a
Pedro Machado <macpedrot@gmail.com>
2023-10-01 21:30:14 +0100

    🎨 add components pending usage

---
+++ src/components/base/PageView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-4 sm:p-0">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ src/components/views/SignIn.vue
@@ -0,0 +1,27 @@
+<template>
+    <PageView>
+        <h1 class="uppercase">Sign in</h1>
+        <form @submit.prevent="" class="flex flex-col">
+            <label for="email">Email:</label>
+            <Input v-model="email" id="email" />
+
+            <label for="password">Password:</label>
+            <Input type="password" v-model="password" id="password" />
+            <Button>Sign in</Button>
+        </form>
+    </PageView>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Input from '../base/Input.vue';
+import Button from '../base/Button.vue';
+import PageView from '../base/PageView.vue';
+
+const isLogin = ref(true)
+const email = ref('')
+const password = ref('')
+
+</script>
+
+<style scoped></style>
