5775b37cf583dfb53e456dfd163edbbc85d22565
Pedro Machado <macpedrot@gmail.com>
2023-10-01 20:50:36 +0100

    💄 add input component

---
+++ src/components/base/Input.vue
@@ -0,0 +1,13 @@
+<template>
+    <input :id="id" :type="type"
+        :class="`border-2 border-gray-500 rounded p-1 leading-3 mb-1 focus:outline-blue-400 pl-2 ${className}`"
+        :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLInputElement)?.value)" />
+    <span v-if="errorMsg" class="text-red-500">{{ errorMsg }}</span>
+</template>
+
+<script setup lang="ts">
+defineProps(['modelValue', 'type', 'id', 'className', 'errorMsg'])
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped></style>
